b900b74c25d1bd0242ad4affb5e9e180b83b95ed
Arthur Buliva <arthur@kamusiproject.org>
2016-05-10 15:52:31 +0300

    A little cleanup

--- src/wikipedia/WikipediaParseTitles.java
+++ src/wikipedia/WikipediaParseTitles.java
@@ -10,15 +10,8 @@ import com.mongodb.client.MongoDatabase;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import nlp.SentenceDetector;
 import org.bson.Document;
 
@@ -92,6 +85,8 @@ public class WikipediaParseTitles
         });
 
         mongoClient.close();
+        
+        BAKITA
     }
 
     public static void main(String[] args) throws Exception
