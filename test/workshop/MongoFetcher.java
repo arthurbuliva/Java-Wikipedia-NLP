3a40de18547e8a2254e7fb3863a80d2a22662e8b
Arthur Buliva <arthur.buliva@unon.org>
2016-09-12 10:25:14 +0300

    Corpus is Ready. We now try Moses

--- test/workshop/MongoFetcher.java
+++ test/workshop/MongoFetcher.java
@@ -1,7 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * MongoFetcher
+ *
+ * @author arthur
+ *
+ * Makes an English - Swahili corpus from Wikipedia entries
  */
 package workshop;
 
@@ -9,70 +11,54 @@ import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import nlp.ChunkFrequency;
-import nlp.Chunker;
-import nlp.EntityFinder;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import nlp.SentenceDetector;
-import nlp.TitleMatcher;
 
 import org.bson.Document;
 
-/**
- * MongoDB Quick Guide
- *
- * @author arthur
- *
- * This is how we insert a record into the db:
- *
- * db.getCollection("test").insertOne( new Document() .append("en",
- * "\"Nakumatt\" is an abbreviation for Nakuru Mattress.[1]") .append("sw",
- * "Nakumatt ni mnyororo wa maduka nchini Kenya.\n" + "Ina maduka 18 kote nchini
- * Kenya [1] na inaajiri watu 3,200.\n" + "Ni mipango ya kupanua maduka yake
- * mpaka nchini Uganda, Rwanda na nchi nyingine za Afrika Mashariki.\n" +
- * "Nakumatt ni kampuni ya Kenya inayomilikiwa na familia na Atul Shah Hotnet
- * Ltd.[2] [3]")); This is how we created an index for the text:
- *
- * db.wikipedia.createIndex({ "en" : "text", "sw" : "text" }, {
- * default_language: "english" });
- *
- * This is how we dump data from MongoDB:
- *
- * mongodump --collection wikipedia --db corpus --out __db/dump
- *
- * This is how we restore from the dump:
- *
- * mongorestore --db corpus --noIndexRestore --drop __db/dump/corpus/
- *
- * Search without index ++++++++++++++++++++ DBQuery.shellBatchSize = 300
- * db.wikipedia.find({"sw": /Adelaide wa Italia/}).pretty();
- * db.wikipedia.find().sort({_id:-1}).pretty().limit(1);
- *
- * Search with index ++++++++++++++++++++ db.wikipedia.find({$text: {$search:
- * "Msimu wa mvua"}}).pretty(); db.wikipedia.find({$text: {$search: "\"Jamhuri
- * ya Kenya\""}}).pretty();
- */
 public class MongoFetcher
 {
 
-    private MongoClient mongoClient;
-    private MongoDatabase db;
+    private final MongoClient MONGO_CLIENT;
+    private final MongoDatabase MONGO_DB;
+    private final String SWAHILI_CORPUS_FILE = "sw.wikipedia.txt";
+    private final String ENGLISH_CORPUS_FILE = "en.wikipedia.txt";
+
+    private FileWriter ENGLISH_STREAM;
+    private BufferedWriter ENGLISH_WRITER;
+
+    private FileWriter SWAHILI_STREAM;
+    private BufferedWriter SWAHILI_WRITER;
+
+    private static final Logger LOGGER = Logger.getLogger(MongoFetcher.class.getName());
 
     public MongoFetcher()
     {
-        mongoClient = new MongoClient();
-        db = mongoClient.getDatabase("corpus");
+        MONGO_CLIENT = new MongoClient();
+        MONGO_DB = MONGO_CLIENT.getDatabase("corpus");
+
+        try
+        {
+            ENGLISH_STREAM = new FileWriter(ENGLISH_CORPUS_FILE, true);
+            ENGLISH_WRITER = new BufferedWriter(ENGLISH_STREAM);
+
+            SWAHILI_STREAM = new FileWriter(SWAHILI_CORPUS_FILE, true);
+            SWAHILI_WRITER = new BufferedWriter(SWAHILI_STREAM);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
     {
         MongoFetcher fetcher = new MongoFetcher();
         fetcher.fetchAll();
-
     }
 
     public void fetchAll()
@@ -80,10 +66,8 @@ public class MongoFetcher
 
         Document projection = new Document("score", new Document("$meta", "textScore"));
 
-        FindIterable<Document> iterable = db.getCollection("wikipedia")
-                .find( //                        new Document("$text", new Document("$search", exactMatch ? String.format("\"%s\"", original) : original)
-                //                        )
-                )
+        FindIterable<Document> iterable = MONGO_DB.getCollection("wikipedia")
+                .find()
                 .projection(projection)
                 .sort(projection)
                 .limit(0);
@@ -113,11 +97,35 @@ public class MongoFetcher
                             SentenceDetector.detectSentences(english)[0]
                             + " => "
                             + SentenceDetector.detectSentences(swahili)[0]);
+
+                    try
+                    {
+
+                        ENGLISH_WRITER.write(SentenceDetector.detectSentences(english)[0]);
+                        ENGLISH_WRITER.newLine();
+
+                        SWAHILI_WRITER.write(SentenceDetector.detectSentences(swahili)[0]);
+                        SWAHILI_WRITER.newLine();
+                    }
+                    catch (IOException ex)
+                    {
+                        LOGGER.log(Level.SEVERE, ex.toString(), ex);
+                    }
+
                 }
 
             }
         });
 
+        try
+        {
+            ENGLISH_WRITER.close();
+            SWAHILI_WRITER.close();
+        }
+        catch (IOException ex)
+        {
+            LOGGER.log(Level.SEVERE, ex.toString(), ex);
+        }
     }
 
 }
