fd8d9734e17d4cc0cf3601584cb5a52fd7534b14
arthur buliva <arthurbuliva@gmail.com>
2016-10-02 16:59:44 +0300

    Refactored and enabled chunking

---
+++ src/MongoDB/MongoDB.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MongoDB;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import java.util.HashMap;
+import org.bson.Document;
+
+/**
+ *
+ * @author arthur
+ */
+public class MongoDB
+{
+
+    private final MongoClient mongoClient;
+    private final MongoDatabase db;
+    private final HashMap<String, String> relationship;
+
+    public MongoDB()
+    {
+        relationship = new HashMap<>();
+        mongoClient = new MongoClient();
+        db = mongoClient.getDatabase("corpus");
+    }
+
+    public HashMap<String, String> fetchFromMongoDB(String original)
+    {
+        Document projection = new Document("score", new Document("$meta", "textScore"));
+
+        HashMap<String, String> data = new HashMap<>();
+
+        try (
+                MongoCursor<Document> cursor = db.getCollection("wikipedia")
+                .find(
+                        new Document("$text",
+                                new Document("$search", String.format("%s", original))
+                                .append("$language", "en")
+                                .append("$caseSensitive", false)
+                        )
+                )
+                .projection(projection)
+                .sort(projection)
+                .limit(0)
+                .iterator())
+        {
+            while (cursor.hasNext())
+            {
+                Document document = cursor.next();
+
+//                System.out.println(Chunker.getSpanTypes(document.getString("en")));
+                String english = document.getString("en");
+                String swahili = document.getString("sw");
+                String title = document.getString("title");
+                String kichwa = document.getString("kichwa");
+
+                data.put(title, kichwa);
+                data.put(english, swahili);
+            }
+        }
+        
+//        mongoClient.close();
+
+        return data;
+    }
+}
--- test/workshop/MongoTranslatorRC1.java
+++ test/workshop/MongoTranslatorRC1.java
@@ -5,34 +5,36 @@
  */
 package workshop;
 
+import MongoDB.MongoDB;
+import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.Keyword;
 import nlp.*;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import opennlp.tools.tokenize.SimpleTokenizer;
-
-import org.bson.Document;
 
 public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopWords, SwahiliStopWords
 {
 
     private final MongoClient mongoClient;
     private final MongoDatabase db;
+    private final MongoDB connection;
     private final HashMap<String, String> relationship;
     int retries = 1;
     private ArrayList<String> translation = new ArrayList<>();
 
     public MongoTranslatorRC1()
     {
+        connection = new MongoDB();
         relationship = new HashMap<>();
         mongoClient = new MongoClient();
         db = mongoClient.getDatabase("corpus");
-
     }
 
     public static void main(String[] args)
@@ -51,15 +53,13 @@ public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopW
 //        String english = "Tomorrow is a Wednesday";
 //        String english = "A gospel is an account describing the life, death, and resurrection of Jesus of Nazareth";
 //        String english = "The Bible is a collection of sacred texts in Judaism and Christianity";
-        
-        
-        
+
 //        String english = "Paper is a thin material";
-//        String english = "The United Nations Security Council (UNSC) is one of the six principal organs of the United Nations";
-//        String english = "A benediction is a short invocation for divine help";
-//        String english = "An avalanche is a rapid flow of snow down a sloping surface";
-//        String english = "A sea is a large body of salt water";
-//        String english = "Augustus was the founder of the Roman Empire and its first Emperor";
+//        String english = "The United Nations Security Council is an organ of The United Nations";
+//        String english = "A benediction is a short prayer";
+//        String english = "An avalanche is a rapid flow of snow down the slope of a hill";
+//        String english = "An ocean is a large body of water";
+//        String english = "Jomo kenyatta was the founder of the Republic of Kenya and its first president";
 //        String english = "An arthropod is an invertebrate animal having an exoskeleton";
 //        String english = "Antoine Hey (born 19 September 1970) is a German football coach and former professional player who played in the Bundesliga";
 //        String english = "The Universal Declaration of Human Rights";
@@ -68,16 +68,16 @@ public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopW
 //        String english = "Virtue is moral excellence";
 //        String english = "\"Don't Forget About Us\" is a song by American singer and songwriter Mariah Carey";
 //        String english = "The United States dollar";
-//        String english = "Sin is the act of violating God's will by transgressing his commandments";
+//        String english = "Sin is disobeying God's will by not following commandments";
 //        String english = "The Islamic State of Iraq and the Levant";
 //        String english = "Culture are habits acquired by man as a member of society";
-//        String english = "A pit latrine or pit toilet is a type of toilet that collects human feces in a hole in the ground.";
+//        String english = "A toilet collects human waste";
 //        String english = "Czechoslovakia was a sovereign state in Central Europe ";
-        String english = "Kenyatta National Hospital";
+        String english = "Barack Obama went to Kenyatta National Hospital";
 
         MongoTranslatorRC1 t = new MongoTranslatorRC1();
 
-        System.out.println(t.translate(english.toLowerCase()));
+        System.out.println(t.translate(english));
 
     }
 
@@ -95,6 +95,14 @@ public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopW
         {
             return (titleMatcher.translate(original.trim()));
         }
+        else if (original.startsWith("an "))
+        {
+            return translate(original.replaceFirst("an", "").trim());
+        }
+        else if (original.startsWith("a "))
+        {
+            return translate(original.replaceFirst("a", "").trim());
+        }
         else if (original.startsWith("of"))
         {
             translation.add("ya ");
@@ -110,67 +118,65 @@ public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopW
             return translate(original.replaceFirst("the", "").trim());
         }
 
-        String parsedKeyWords = original;
+        log(Level.INFO, "Searching MongoDB: " + original);
 
-        System.out.println("Searching MongoDB: " + parsedKeyWords);
-
-        HashMap<String, String> mongoData = fetchFromMongoDB(String.format("\"%s\"", new Object[]
+        HashMap<String, String> mongoData = connection.fetchFromMongoDB(String.format("\"%s\"", new Object[]
         {
-            parsedKeyWords
+            original
         }));
 
-        if (mongoData.isEmpty())
+        if (mongoData.isEmpty()) // Whole phrase not in Wikipedia. We break it down
         {
-            ArrayList<String> whole = new BetaStringBreaker().breakString(original);
-//
-//            System.out.println("Segmented: " + whole);
 
-            for (String segment : whole)
+            for (String segment : Chunker.chunk(original))
             {
+                String subTranslation = translate(segment.trim());
 
-//                System.out.println(segment);
                 log(Level.INFO, "Empty segment. Translating -> " + (segment));
+
+                if (subTranslation.isEmpty())
+                {
+                    System.out.println(removeStopWords(segment));
+                    
+                    String segmentTranslation = translate(removeStopWords(segment));
+
+                    if (!segmentTranslation.contains("|"))
+                    {
+                        translation.add(segmentTranslation);
+                    }
+                    
+                }
+                else
+                {
+                    System.out.println(segment);
+
+                    String segmentTranslation = translate(segment);
+
+                    if (!segmentTranslation.contains("|"))
+                    {
+                        translation.add(segmentTranslation);
+                    }
+                }
+
                 System.gc();
-                translation.add(translate(segment));
             }
         }
-        else
+        else // Whole phrase exists in Wikipedia
         {
-//            SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
-//                        String[] wordTokens = tokenizer.tokenize(removeStopWords(original));
-//
-//            for (String entry : wordTokens)
-//            {
-//                System.out.println(entry);
-//            }
-
             for (Map.Entry<String, String> entry : mongoData.entrySet())
             {
-//                System.out.println(entry.getKey() + " => " + entry.getValue());
-
                 String[] englishSentences = SentenceDetector.detectSentences(entry.getKey());
                 String[] swahiliSentences = SentenceDetector.detectSentences(entry.getValue());
-
-//                System.out.println(englishSentences.length);
-//                System.out.println(swahiliSentences.length);
 
                 if (englishSentences[0].toLowerCase().trim().contains(original.toLowerCase().trim()))
                 {
                     return swahiliSentences[0];
                 }
-
-                for (String sentence : englishSentences)
+                else
                 {
-//                    System.out.println(sentence);
+                    return Arrays.toString(swahiliSentences);
                 }
-                for (String sentensi : swahiliSentences)
-                {
-//                    System.out.println(sentensi);
-                }
-
-//                System.out.println(Arrays.toString(SentenceDetector.detectSentences(entry.getKey())));
             }
-
         }
 
         HashMap<String, Integer> occurranceCount = new HashMap<>();
@@ -187,12 +193,10 @@ public class MongoTranslatorRC1 extends TranslatorLogger implements EnglishStopW
                 occurranceCount.put(s, 1);
             }
         }
-        
+
 //        System.out.println(ChunkFrequency.sortByValue(occurranceCount));
-
 //        return translation.toString();
-
-return ChunkFrequency.sortByValue(occurranceCount).toString();
+        return ChunkFrequency.sortByValue(occurranceCount).toString();
     }
 
     private String removeStopWords(String sentence)
@@ -216,43 +220,5 @@ return ChunkFrequency.sortByValue(occurranceCount).toString();
 
 //            System.out.printf("%s cleaned to %s\n", sentence, clean.toString().trim());
         return clean.toString().trim();
-    }
-
-    private HashMap<String, String> fetchFromMongoDB(String original)
-    {
-        Document projection = new Document("score", new Document("$meta", "textScore"));
-
-        HashMap<String, String> data = new HashMap<>();
-
-        try (
-                MongoCursor<Document> cursor = db.getCollection("wikipedia")
-                .find(
-                        new Document("$text",
-                                new Document("$search", String.format("%s", original))
-                                .append("$language", "en")
-                                .append("$caseSensitive", false)
-                        )
-                )
-                .projection(projection)
-                .sort(projection)
-                .limit(0)
-                .iterator())
-        {
-            while (cursor.hasNext())
-            {
-                Document document = cursor.next();
-
-//                System.out.println(Chunker.getSpanTypes(document.getString("en")));
-                String english = document.getString("en");
-                String swahili = document.getString("sw");
-                String title = document.getString("title");
-                String kichwa = document.getString("kichwa");
-
-                data.put(title, kichwa);
-                data.put(english, swahili);
-            }
-        }
-
-        return data;
     }
 }
